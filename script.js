801f11517c7d5bf3b7857d18ae18d854ff6218bb
coderobel <robel.ugr-4552-16@aau.edu.et>
2025-08-01 09:56:10 +0300

    retrieved user inputs

--- script.js
+++ script.js
@@ -1,4 +1,11 @@
 document.addEventListener("DOMContentLoaded", function (){
     form = document.getElementById("registration-form")
-    feedbackDiv = document.getElementById("form-feedback")})
+    feedbackDiv = document.getElementById("form-feedback")
+    form.addEventListener("submit", function(event){
+        event.preventDefault();
+        const email = document.getElementById("email").value.trim();
+        const password = document.getElementById("password").value.trim();
+        const username = document.getElementById("username").value.trim();
+
+    })})
 
