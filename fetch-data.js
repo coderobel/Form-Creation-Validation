20004d5599f93849bdc80a6bfd32a1de4eb36e29
coderobel <robel.ugr-4552-16@aau.edu.et>
2025-08-02 00:26:30 +0300

    created fetchUserData function

--- fetch-data.js
+++ fetch-data.js
@@ -1,1 +1,9 @@
-
+async function fetchUserData() {
+    const apiUrl = "https://jsonplaceholder.typicode.com/users";
+    const dataContainer = document.getElementById('api_data');
+    try{
+        const response = await fetch(apiUrl);
+        const users = await response.json();
+        dataContainer.innerHTML = '';
+    }
+};
